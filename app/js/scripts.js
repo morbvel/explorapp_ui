247fbaf59b8f3f09966804e376891a1e3c2716b2
Miguel <morbvel@me.com>
2018-02-09 21:00:45 +0100

    Home page styled

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,4 @@
-
+$(document).ready(function () {
+    $('.first-parallax-block').parallax({imageSrc: '../image/parallax1.jpg'});
+    $('.second-parallax-block').parallax({imageSrc: '../image/parallax2.jpeg'});
+});
