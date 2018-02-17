70fc8b918521b299426a5f229b2026a46196e7fe
Miguel Orbañanos Vela <miguel@MacBook-Pro-de-Miguel.local>
2018-02-17 10:22:22 +0100

    Foodies layout completed

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
     $('.first-parallax-block').parallax({imageSrc: '../image/parallax1.jpg'});
     $('.second-parallax-block').parallax({imageSrc: '../image/parallax2.jpeg'});
+
+    $('.first-parallax-block-fodies').parallax({imageSrc: '../image/parallax-foodies-1.jpeg'});
+
+
+    $('.gallery-open').find('img').each(function(){
+       $(this).click(function(e){
+           e.preventDefault();
+
+           $('#modal-image').attr('src', $(this).attr('src'));
+           $('#gallery-modal').modal("show");
+       });
+    });
 });
 
 function initMap(){
